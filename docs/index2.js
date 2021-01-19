f03d7348539fc06abc156ab6cf928f1ab968e496
Joop Ringelberg <joop.ringelberg@gmail.com>
2021-01-19 17:16:51 +0100

    fetch experiment in service worker

---
+++ docs/index2.js
@@ -0,0 +1,34 @@
+if ('serviceWorker' in navigator) {
+
+     // Override the default scope of '/' with './', so that the registration applies
+     // to the current directory and everything underneath it.
+     navigator.serviceWorker.register('service-worker.js', {scope: './'}).then(function(registration) {
+       // At this point, registration has taken place.
+       // The service worker will not handle requests until this page and any
+       // other instances of this page (in other tabs, etc.) have been
+       // closed/reloaded.
+
+       var serviceWorker;
+       if (registration.installing) {
+         serviceWorker = registration.installing;
+       } else if (registration.waiting) {
+         serviceWorker = registration.waiting;
+       } else if (registration.active) {
+         serviceWorker = registration.active;
+       }
+
+       if (serviceWorker) {
+         console.log(serviceWorker.state);
+         serviceWorker.addEventListener('statechange', function(e) {
+           console.log(e.target.state);
+         });
+       }
+     }).catch(function(error) {
+       // Something went wrong during registration. The service-worker.js file
+       // might be unavailable or contain a syntax error.
+       console.log( 'failed: ' + error);
+     });
+   } else {
+     // The current browser doesn't support service workers.
+     console.log("no service workers supported");
+   }
---
+++ docs/service-worker.js
@@ -0,0 +1,12 @@
+
+let myHeaders = new Headers();
+myHeaders.append('Authenticate', 'Basic joop:geheim');
+
+fetch('https://127.0.0.1:6984/_session',
+  { credentials: "include",
+    headers: myHeaders,
+    mode: "cors"})
+  .then( function()
+{
+  fetch( "https://127.0.0.1:6984/localusers/joop");
+});
