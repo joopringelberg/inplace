9d38bfec4a5ac60abbce56c7fe952ed2e48fb772
Joop Ringelberg <joop.ringelberg@gmail.com>
2021-01-19 19:52:02 +0100

    fixed authentication for couchdb

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -3,12 +3,12 @@
 onmessage =
   function()
   {
-    let myHeaders = new Headers();
-    myHeaders.append('Authenticate', 'Basic joop:geheim');
+    const body = {name: "joop", password: "geheim"};
 
     fetch('https://127.0.0.1:6984/_session',
-      { credentials: "include",
-        headers: myHeaders,
+      { method: "POST",
+        credentials: "include",
+        body: JSON.stringify( body ),
         mode: "cors"})
       .then( function()
     {
