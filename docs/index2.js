1e17376eeef7b1cdb2b10878f9022926ebf3a5e9
Joop Ringelberg <joop.ringelberg@gmail.com>
2021-01-19 20:02:32 +0100

    approach couchdb from main script

--- docs/index2.js
+++ docs/index2.js
@@ -23,7 +23,7 @@ if ('serviceWorker' in navigator) {
          serviceWorker.addEventListener('statechange', function(e) {
            console.log(e.target.state);
          });
-         serviceWorker.postMessage('authenticate');
+         // serviceWorker.postMessage('authenticate');
        }
      }).catch(function(error) {
        // Something went wrong during registration. The service-worker.js file
@@ -34,3 +34,24 @@ if ('serviceWorker' in navigator) {
      // The current browser doesn't support service workers.
      console.log("no service workers supported");
    }
+
+const test =
+   function()
+   {
+     const body = {name: "joop", password: "geheim"};
+     let myHeaders = new Headers();
+     myHeaders.append('Content-Type', 'application/json');
+     fetch('https://127.0.0.1:6984/_session',
+       { method: "POST",
+         credentials: "include",
+         headers: myHeaders,
+         body: JSON.stringify( body ),
+         mode: "cors"})
+       .then( function()
+     {
+       fetch( "https://127.0.0.1:6984/localusers/joop");
+     });
+
+   };
+
+test();
