16496b30ccebd6951dab5bbdc84f153c9c47291d
Joop Ringelberg <joop.ringelberg@gmail.com>
2021-01-19 19:58:42 +0100

    added Content-Type header

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -4,10 +4,12 @@ onmessage =
   function()
   {
     const body = {name: "joop", password: "geheim"};
-
+    let myHeaders = new Headers();
+    myHeaders.append('Content-Type', 'application/json');
     fetch('https://127.0.0.1:6984/_session',
       { method: "POST",
         credentials: "include",
+        headers: myHeaders,
         body: JSON.stringify( body ),
         mode: "cors"})
       .then( function()
