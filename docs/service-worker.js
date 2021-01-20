3224f1f086ccfaf985018d9d9bb3f9e4dc7b89c5
Joop Ringelberg <joop.ringelberg@gmail.com>
2021-01-20 10:49:12 +0100

    fixed test

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,55 +1,53 @@
 
 
-// onmessage =
-//   function()
-//   {
-//     const body = {name: "joop", password: "geheim"};
-//     let myHeaders = new Headers();
-//     myHeaders.append('Content-Type', 'application/json');
-//     fetch('https://127.0.0.1:6984/_session',
-//       { method: "POST",
-//         credentials: "include",
-//         headers: myHeaders,
-//         body: JSON.stringify( body ),
-//         mode: "cors"})
-//       .then( function()
-//     {
-//       fetch( "https://127.0.0.1:6984/localusers/joop", {credentials: "include"});
-//     });
-//
-//   };
-
-onmessage = function ()
-{
-  const body = {name: "joop", password: "geheim"};
-  const xhr = new XMLHttpRequest();
-  xhr.open("POST", "https://127.0.0.1:6984/_session", true, "joop", "geheim");
-  xhr.setRequestHeader('Content-Type', 'application/json');
-  xhr.onerror = function(msg){("AJAX request failed" + msg);};
-  xhr.ontimeout = function(msg){("AJAX request timed out" + msg);};
-  // xhr.responseType = options.responseType;
-  xhr.withCredentials = true;
-  xhr.send( JSON.stringify( body ) );
-  xhr.onload = function ()
+onmessage =
+  function()
+  {
+    const isBrowser=new Function("try {return this===window;}catch(e){ return false;}");
+    if (isBrowser())
     {
-      const xhr = new XMLHttpRequest();
-      xhr.open("GET", "https://127.0.0.1:6984/localusers/joop");
-      xhr.onerror = function(msg){("AJAX request failed" + msg);};
-      xhr.ontimeout = function(msg){("AJAX request timed out" + msg);};
-      // xhr.responseType = options.responseType;
-      xhr.withCredentials = true;
-      xhr.send();
-      xhr.onload = function ()
-        {
+      console.log("isBrowser returns true in service worker")
+    }
+    const body = {name: "joop", password: "geheim"};
+    let myHeaders = new Headers();
+    myHeaders.append('Content-Type', 'application/json');
+    fetch('https://127.0.0.1:6984/_session',
+      { method: "POST",
+        credentials: "include",
+        headers: myHeaders,
+        body: JSON.stringify( body ),
+        mode: "cors"})
+      .then( function()
+    {
+      fetch( "https://127.0.0.1:6984/localusers/joop", {credentials: "include"});
+    });
 
-        };
+  };
 
-    };
-};
-
-var isBrowser=new Function("try {return this===window;}catch(e){ return false;}");
-
-if (isBrowser())
-{
-  console.log("isBrowser returns true in service worker")
-}
+// onmessage = function ()
+// {
+//   const body = {name: "joop", password: "geheim"};
+//   const xhr = new XMLHttpRequest();
+//   xhr.open("POST", "https://127.0.0.1:6984/_session", true, "joop", "geheim");
+//   xhr.setRequestHeader('Content-Type', 'application/json');
+//   xhr.onerror = function(msg){("AJAX request failed" + msg);};
+//   xhr.ontimeout = function(msg){("AJAX request timed out" + msg);};
+//   // xhr.responseType = options.responseType;
+//   xhr.withCredentials = true;
+//   xhr.send( JSON.stringify( body ) );
+//   xhr.onload = function ()
+//     {
+//       const xhr = new XMLHttpRequest();
+//       xhr.open("GET", "https://127.0.0.1:6984/localusers/joop");
+//       xhr.onerror = function(msg){("AJAX request failed" + msg);};
+//       xhr.ontimeout = function(msg){("AJAX request timed out" + msg);};
+//       // xhr.responseType = options.responseType;
+//       xhr.withCredentials = true;
+//       xhr.send();
+//       xhr.onload = function ()
+//         {
+//
+//         };
+//
+//     };
+// };
