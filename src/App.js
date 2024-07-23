ac23fdbc78bea61163b5425dbdc446ec21cc882b
Joop Ringelberg <joop.ringelberg@gmail.com>
2024-07-23 09:34:03 +0200

    We can now open a context by providing its (fully qualified) indexed name.

--- src/App.js
+++ src/App.js
@@ -929,6 +929,13 @@ export default class App extends Component
 }
 
 // This function returns a promise for the external role of the context of the role s that is passed in, or fails.
+// It expands default namespaces and dereferences IndexedContext names.
+// If the role is a context role, returns the binding of that role.
+//  - pass through an external role
+//  - expand namespace and dereference indexed contexts
+//  - construct an external role for a context identifier
+//  - return the context of the filler for a role identifier.
+//  - throw an error in all other cases.
 function ensureExternalRole(s)
 {
   if ( isExternalRole( s ) )
@@ -941,36 +948,48 @@ function ensureExternalRole(s)
     return PDRproxy.then( proxy =>
       new Promise( function( resolve, reject )
         {
-          proxy.getBinding( s,
-            function (bindingIds)
+          proxy.matchContextName( s, 
+            ( function (serialisedtable)
               {
-                if (bindingIds.length > 0)
+                const table = JSON.parse( serialisedtable[0]);
+                if (table[s])
                 {
-                  if ( isExternalRole (bindingIds[0]))
-                  {
-                    resolve( bindingIds[0] );
-                  }
-                  else
-                  {
-                    proxy.getRolContext( bindingIds[0] ).then(
-                      contextArr => resolve( externalRole( contextArr[0] ))
-                    )
-                  }
+                  resolve( externalRole( table[s] ))
                 }
                 else
                 {
-                  // Otherwise, either not a context role after all, or no binding. Fail.
-                  return reject( new Error( "This role is not an external role and has no filler either, so cannot open a context for role: " + s ));
+                  proxy.getBinding( s,
+                    function (bindingIds)
+                      {
+                        if (bindingIds.length > 0)
+                        {
+                          if ( isExternalRole (bindingIds[0]))
+                          {
+                            resolve( bindingIds[0] );
+                          }
+                          else
+                          {
+                            proxy.getRolContext( bindingIds[0] ).then(
+                              contextArr => resolve( externalRole( contextArr[0] ))
+                            )
+                          }
+                        }
+                        else
+                        {
+                          // Otherwise, either not a context role after all, or no binding. Fail.
+                          return reject( new Error( "This role is not an external role and has no filler either, so cannot open a context for role: " + s ));
+                        }
+                      },
+                    FIREANDFORGET,
+                    function (e)
+                    {
+                      return reject(e);
+                    });
                 }
-              },
-            FIREANDFORGET,
-            function (e)
-            {
-              return reject(e);
-            });
+              })
+            , FIREANDFORGET );
         }));
   }
-
 }
 
 // A function that generates a CUID using the current epoch as fingerprint.
